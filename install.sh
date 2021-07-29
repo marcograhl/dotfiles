c74b576853b3b81ece14aeed055912744e6b1395
Issy Long <me@issyl0.co.uk>
2021-07-29 11:31:49 +0100

    install: Copy around `.gitignore`, `.gitconfig`

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ set -x
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/.vimrc $HOME/.vimrc
 ln -s $(pwd)/.vim $HOME/.vim
+ln -s $(pwd)/.gitignore_global $HOME/.gitignore_global
+ln -s $(pwd)/.gitconfig $HOME/.gitconfig
+ln -s $(pwd)/.gitconfig-github $HOME/.gitconfig-github
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
