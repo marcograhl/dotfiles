ba6755657ded25e8c0c95c184e6ee1a290c599b5
Issy Long <me@issyl0.co.uk>
2022-10-28 10:48:48 +0100

    install: Actually install nvim!

--- install.sh
+++ install.sh
@@ -10,27 +10,24 @@ if [[ -z "${CODESPACES}" ]]; then
 else
   echo "==> installing dotfiles in codespace"
   sudo apt-get update
-  sudo apt-get -y install fd-find python3-dev python3-pip tmux
-  sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat ripgrep
-  pip3 install --user pynvim
-  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-  chmod u+x nvim.appimage
-  ./nvim.appimage --appimage-extract
-  ./squashfs-root/AppRun --version
-  sudo mv squashfs-root / && mkdir -p $HOME/.local/bin/ && ln -s /squashfs-root/AppRun $HOME/.local/bin/nvim
+  sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat
+
+  sudo modprobe fuse
+  sudo groupadd fuse
+  sudo usermod -a -G fuse "$(whoami)"
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  sudo chmod u+x nvim.appimage
+  sudo mv nvim.appimage /usr/local/bin/nvim
 fi
 
-mkdir -p ~/.tmux/plugins/
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mkdir -p ~/.config/nvim/autoload/
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+mkdir -p $(pwd)/.config/nvim/
 ln -sf $(pwd)/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $(pwd)/.gitignore_global $HOME/.gitignore_global
 ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/.gitconfig-github $HOME/.gitconfig-github
-ln -sf $(pwd)/starship.toml $HOME/.config/starship.toml
 
 if [[ -z "${CODESPACES}" ]]; then
   echo "not on Codespaces, don't need to unset git config for GPG signing"
