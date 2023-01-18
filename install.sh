4b78a5057051d0cccb7baf515d4c33d9b15fa274
marcograhl <20419551+marcograhl@users.noreply.github.com>
2023-01-18 09:27:00 +0000

    testing setup

--- install.sh
+++ install.sh
@@ -4,41 +4,23 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-if [[ -z "${CODESPACES}" ]]; then
-  echo "==> assuming installing on macOS"
-  brew bundle
-else
-  echo "==> installing dotfiles in codespace"
-  sudo apt-get update
-  sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat
 
-  sudo modprobe fuse
-  sudo groupadd fuse
-  sudo usermod -a -G fuse "$(whoami)"
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  sudo chmod u+x nvim.appimage
-  sudo mv nvim.appimage /usr/local/bin/nvim
-fi
+sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat
 
-mkdir -p ~/.config/nvim/autoload/
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo modprobe fuse
+sudo groupadd fuse
+sudo usermod -a -G fuse "$(whoami)"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+sudo apt install fuse
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 mkdir -p $(pwd)/.config/nvim/
-ln -sf $(pwd)/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $(pwd)/.config/nvim/init.lua $HOME/.config/nvim/init.lua
 ln -sf $(pwd)/.gitignore_global $HOME/.gitignore_global
-ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
-ln -sf $(pwd)/.gitconfig-github $HOME/.gitconfig-github
 
-if [[ -z "${CODESPACES}" ]]; then
-  echo "not on Codespaces, don't need to unset git config for GPG signing"
-else
-  git config --file ~/.gitconfig --unset user.email
-  git config --file ~/.gitconfig --unset user.signingkey
-  git config --file ~/.gitconfig --unset gpg.program
-fi
-
-if [ -d "/workspaces/github" ]; then
-  git -C /workspaces/github config gpg.program /.codespaces/bin/gh-gpgsign
-fi
-
-nvim --headless +PlugInstall +qa
+# Auto install packer plugins to avoid yelling errors on first boot
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
