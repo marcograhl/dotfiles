8beff4097af23b9029fae5e5379c9e2ad95f3102
marcograhl <20419551+marcograhl@users.noreply.github.com>
2023-01-19 11:25:07 +0000

    waht

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
-sudo wget -O mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkce
+sudo wget -O mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir -p $HOME/.config/nvim/
